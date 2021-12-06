0a63d12bbe87799fcaec1d1b6e7fd511329df759
ShinWolf <eric.du62110@gmail.com>
2021-12-06 11:59:04 +0100

    fini

--- src/Form/AjoutCompetenceType.php
+++ src/Form/AjoutCompetenceType.php
@@ -6,15 +6,15 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AjoutCompetenceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('public', CheckboxType::class, [
-            'label'    => 'java ',
-            'required' => false,
-        ]);
+        $builder
+        ->add('Valider', SubmitType::class)
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
